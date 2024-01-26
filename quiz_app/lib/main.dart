3be84c5cc213bd967f64bedc3422afd11b12913a
DanielSiilva <daniellsousa0614@gmail.com>
2024-01-26 10:07:01 -0300

    entendo como passar funcoes, renderizar varios elementos  com o column e evento onpress no botoao

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -3,14 +3,38 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta respondida');
+  }
+
   @override
+  final List<String> perguntas = [
+    'Qual a sua cor favorita?',
+    'Qual a sua linguagem de programacao favorita?'
+  ];
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Ola flutter !!'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Botao 01'),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text('Botao 02'),
+              onPressed: () {
+                print('Resposta 02');
+              },
+            ),
+            ElevatedButton(
+                child: Text('Botao 03'), onPressed: () => print('Funcao 03'))
+          ],
+        ),
       ),
     );
   }
