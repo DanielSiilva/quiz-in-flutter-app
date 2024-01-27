1ad6cf72cadbc44ed57e9f530d07d922ed0634bb
DanielSiilva <daniellsousa0614@gmail.com>
2024-01-27 18:04:46 -0300

    exibindo resultados

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -65,7 +65,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   ...respostas.map((t) => Resposta(t, _responder)).toList()
                 ],
               )
-            : null,
+            : Center(
+                child: Text(
+                  'Parabéns',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
